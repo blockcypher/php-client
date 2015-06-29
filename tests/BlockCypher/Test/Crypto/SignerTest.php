0be196446cfc7c8ea30273a78f114a79a72aa4a0
Jose Celano <josecelano@gmail.com>
2015-06-29 10:57:55 +0100

    Missing @test annotation

--- tests/BlockCypher/Test/Crypto/SignerTest.php
+++ tests/BlockCypher/Test/Crypto/SignerTest.php
@@ -15,14 +15,22 @@ class SignerTest extends \PHPUnit_Framework_TestCase
     const TO_SIGN = 'd494ba34f5b2938aade8ac156d8099ba686f8b68de43e98d5fa35e367b6431f4';
     const SIGNATURE = '304402207b294405cf94117ec3ba2755d752712578180f486d3d4d12a112e7b2cfbfc88c02200e2dcc83ca8488eaf5a698fc04d5ef4daa99440cbc31b622ac7af7fe712c1e16';
 
-    public function should_allow_sign_data_with_hex_private_key()
+    /**
+     * @test
+     */
+    public function
+    should_allow_sign_data_with_hex_private_key()
     {
         $signature = Signer::sign(self::TO_SIGN, self::PRIVATE_KEY);
 
         $this->assertEquals(self::SIGNATURE, $signature);
     }
 
-    public function should_sign_data_deterministically()
+    /**
+     * @test
+     */
+    public function
+    should_sign_data_deterministically()
     {
         $signature1 = Signer::sign(self::TO_SIGN, self::PRIVATE_KEY);
         $signature2 = Signer::sign(self::TO_SIGN, self::PRIVATE_KEY);
@@ -30,7 +38,11 @@ class SignerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(self::SIGNATURE, $signature1, $signature2);
     }
 
-    public function should_allow_sign_data_with_private_key()
+    /**
+     * @test
+     */
+    public function
+    should_allow_sign_data_with_private_key()
     {
         $privateKey = PrivateKeyManipulator::importPrivateKey(self::PRIVATE_KEY);
 
@@ -39,7 +51,11 @@ class SignerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(self::SIGNATURE, $signature);
     }
 
-    public function should_allow_sign_multiple_data_at_once()
+    /**
+     * @test
+     */
+    public function
+    should_allow_sign_multiple_data_at_once()
     {
         $toSign = array(self::TO_SIGN);
 
@@ -54,7 +70,8 @@ class SignerTest extends \PHPUnit_Framework_TestCase
      * @expectedException \InvalidArgumentException
      * @param $toSign
      */
-    public function should_only_allow_sign_multiple_data_from_an_array($toSign)
+    public function
+    should_only_allow_sign_multiple_data_from_an_array($toSign)
     {
         Signer::signMultiple($toSign, self::PRIVATE_KEY);
     }
